4b63d06c7c7dfc1a04b366d8efb17a7913088ec0
Ajay kumar <kajay919878@gmail.com>
2023-06-24 15:57:04 +0530

    add looping conduction

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-gsap.to(".imagecontainer", {
-        ease: Expo.easeInOut,
-        width: "100%",
-        stagger: 2
-})
+var tl = gsap.timeline({
+    repeat: -1
+});
 
-gsap.to(".text h1", {
+tl
+.to(".imagecontainer", {
     ease: Expo.easeInOut,
-    stagger: 2,
-    top: 0
-})
+    width: "100%",
+    stagger: 2
+}, 'a')
 
-gsap.to(".text h1", {
-    delay: 2,
-    ease: Expo.easeInOut,
-    stagger: 2,
-    top: "-100%"
-})
+.to(".text h1", {
+ease: Expo.easeInOut,
+stagger: 2,
+top: 0
+}, 'a')
+
+.to(".text h1", {
+delay: 2,
+ease: Expo.easeInOut,
+stagger: 2,
+top: "-100%"
+}, 'a')
+
